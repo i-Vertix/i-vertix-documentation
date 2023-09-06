cc77ab98ff990ec60a17418782cd8e35b7b63ed9
flex <felix.geier@pgum.eu>
2023-09-06 11:18:49 +0200

    test environment variable for dev deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
 
     // Set the production url of your site here
     // url: 'https://i-vertix.guide',
-    url: 'https://i-vertix-docs-dev.netlify.app',
+    url: process.env.REACT_APP_URL ?? 'https://i-vertix.guide',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
